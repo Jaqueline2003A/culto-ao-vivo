289963a79a798d221ea8e07303d313cfbbf3b3c9
live <148585779+Jaqueline2003A@users.noreply.github.com>
2026-08-23 18:57:32 -0500

    Update YouTube live link and improve formatting

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // LINK DA LIVE DO YOUTUBE
 
 const LINK_DA_LIVE =
-    "https://www.youtube.com/live/ZlePerquyAk?is=SuZVQWVfmWSLugQD";
+    "https://www.youtube.com/live/ZlePerquyAk?si=MHlGRZ7hTTOB2suj";
 
 
 // URL DO GOOGLE APPS SCRIPT
