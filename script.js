f193613c89b3b5610c972965310765507c597324
live <148585779+Jaqueline2003A@users.noreply.github.com>
2026-08-23 08:13:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // LINK DA LIVE DO YOUTUBE
 
 const LINK_DA_LIVE =
-    "https://www.youtube.com/live/YOy8aPUH6cs?is=XGxLhk_jZfxgJTET";
+    "https://www.youtube.com/live/YOy8aPUH6cs?si=1H32M-EaBKxoA4ir";
 
 
 // URL DO GOOGLE APPS SCRIPT
